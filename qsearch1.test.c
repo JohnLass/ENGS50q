c7b027152602d710e8264887140345440ac8426d
BobbyCarangelo <robert.f.carangelo.iii.21@dartmouth.edu>
2020-10-11 13:01:21 -0400

    10.11.2020 1:00 PM EST

--- qsearch1.test.c
+++ qsearch1.test.c
@@ -17,7 +17,7 @@
 #include <stdbool.h>
 
 int main(void){
-	queue_t *q;
+	queue_t *q, *q2;
 	car_t *p1 = makecar("car1", 28.3, 2800);
 	car_t *p2 = makecar("car2", 128.3, 800);
 	car_t *p3 = makecar("car3", 8.3, 22800);
@@ -26,6 +26,7 @@ int main(void){
 	
 	fn = search_plate;
 	q = qopen();
+	q2 = qopen();
 	
 	qput(q, p1);
 	qput(q, p2);
@@ -40,6 +41,15 @@ int main(void){
 
 	print_node((void *)testp);
 	qclose(q);
+
 	
+	testp = qsearch(q2,fn,"car3");
+
+	if(testp != NULL){
+		printf("failed");
+		exit(EXIT_FAILURE);
+	}
+
+	qclose(q2);
 	exit(EXIT_SUCCESS);
 }
